ba8024bbdd2c859e05301bbf9ae01da20f55318d
PARI PATEL <paripatel3654@gmail.com>
2025-06-13 22:26:46 +0530

    Create star.java

---
+++ PATTERNS/Part1/star.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Basics {
+    public static void main(String args[]) {
+      int n =6;
+    for (int line=1; line<=n; line++){
+       for(int stars=1; stars<=line; stars++) {
+        System.out.print("*");
+       }
+       System.out.println();
+    } 
+    }
+   }
+    
