7af89150cff25cc4044ca60e7563d4772b6fe611
PARI PATEL <paripatel3654@gmail.com>
2025-06-28 20:00:59 +0530

    Create inverted_pyramid.java

---
+++ PATTERNS/Part2/inverted_pyramid.java
@@ -0,0 +1,22 @@
+public class Basics {
+   public static void inverted_pyramid(int n ){
+    //outer 
+    for(int i=1; i<=n; i++) {
+        //spaces
+        for(int j=1; j<=n-i; j++) {
+            System.out.print(" ");
+        }
+
+        //stars
+      for(int j=1; j<=i; j++){
+        System.out.print("*");
+      }
+
+      System.out.println();
+    }
+   }
+   
+    public static void main(String args[]) {
+     inverted_pyramid(4);
+    }
+}
