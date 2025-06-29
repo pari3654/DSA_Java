c4906677de45816e8b6974a5fe19c0261afc9815
PARI PATEL <paripatel3654@gmail.com>
2025-06-29 19:36:56 +0530

    Create halfpyramid_no.java

---
+++ PATTERNS/Part2/halfpyramid_no.java
@@ -0,0 +1,16 @@
+public class Basics {
+  public static void inverted_no(int n ){
+    for (int i=1; i<=n; i++) {
+      //inner- numbers
+      for(int j=1; j<=n-i+1; j++) {
+        System.out.print(j + " ");
+      }
+       System.out.println();
+    }
+    
+  }
+   
+    public static void main(String args[]) {
+     inverted_no(5);
+    }
+}
