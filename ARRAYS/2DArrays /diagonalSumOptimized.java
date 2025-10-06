9c97c330694553f0e5a902e97872338bad7bae5b
PARI PATEL <paripatel3654@gmail.com>
2025-10-06 20:33:25 +0530

    diagonalSumOptimized.java

--- ARRAYS/2DArrays /diagonalSumOptimized.java
+++ ARRAYS/2DArrays /diagonalSumOptimized.java
@@ -1,21 +1,20 @@
 import java.util.*;
 
 public class Array {
-   public static int diagonalSum(int matrix[][]) {
+   
+     public static int diagonalSum(int matrix[][]) {
      int sum = 0;
-     for(int i=0; i<matrix.length; i++ ){
-      for(int j =0; j<matrix[0].length; j++) {
-        //primary diagonal
-        if(i == j) {
-          sum += matrix[i][j];
-        } //secondary diagonal  
-        else if(i+j == matrix.length-1) {
-          sum += matrix[i][j];
-        }
-      }
+     for(int i=0; i<matrix.length; i++) {
+     //primary diagonal 
+     sum += matrix[i][i];
+     //secondary diagonal 
+     if(i != matrix.length-1-i)
+     sum += matrix[i][matrix.length-i-1];
      }
+
      return sum;
    }
+
 
      public static void main(String args[]) {
        int matrix[][] = {{1,2,3,4} ,
