83836d77796a91939d58795f30dbadf06dd222b0
PARI PATEL <paripatel3654@gmail.com>
2025-10-06 23:42:05 +0530

    Update staircaseSearch.java

--- ARRAYS/2DArrays /staircaseSearch.java
+++ ARRAYS/2DArrays /staircaseSearch.java
@@ -1,31 +1,42 @@
 import java.util.*;
 
 public class Array {
-  public static boolean staircaseSearch(int matrix[][], int key){                                             
-  int row = 0, col = matrix[0].length-1;
+    // Function to perform Staircase Search
+    public static boolean staircaseSearch(int matrix[][], int key) {
+        // Start from top-right corner
+        int row = 0;
+        int col = matrix[0].length - 1;
 
-  while(row< matrix.length && col >=0) {
-    if(matrix[row][col] == key) {
-      System.out.println("found key at (" + row+ ", " + col+ ") ");
-      return true;
-      }
-      else if (key < matrix[row][col]) {
-        col--;
-      }
-      else {
-        row++;
-      }
+        // Loop until we go out of matrix bounds
+        while (row < matrix.length && col >= 0) {
+            //  Check if current element is the key
+            if (matrix[row][col] == key) {
+                System.out.println("Found key at (" + row + ", " + col + ")");
+                return true;
+            }
+            // If key is smaller, move left (col--)
+            else if (key < matrix[row][col]) {
+                col--;
+            }
+            //  If key is larger, move down (row++)
+            else {
+                row++;
+            }
+        }
+        // If we reach here, element not found
+        System.out.println("Key not found!");
+        return false;
     }
-    System.out.println("key not found !");
-     return false;
-  }
 
-     public static void main(String args[]) {
-       int matrix[][] = {{10,20,30,40} ,
-                         {15,25,35,45} ,
-                         {27,29,37,48} ,
-                         {32,33,39,50}};
-      int key = 33;
-      staircaseSearch(matrix , key);
-     } 
+    public static void main(String args[]) {
+        int matrix[][] = {
+            {10, 20, 30, 40},
+            {15, 25, 35, 45},
+            {27, 29, 37, 48},
+            {32, 33, 39, 50}
+        };
+
+        int key = 33;
+        staircaseSearch(matrix, key);
+    }
 }
