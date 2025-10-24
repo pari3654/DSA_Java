7bd51cd8575c2d4b9e7b4218260ce24e4c1b3676
PARI PATEL <paripatel3654@gmail.com>
2025-10-24 16:50:43 +0530

    Create vowels.java

---
+++ STRINGS/vowels.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str = new Scanner(System.in).next(); // input string
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+
+        System.out.println("Count of vowels is: " + count);
+    }
+}
+ 
