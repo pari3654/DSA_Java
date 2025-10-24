0140a33a2bafba8063e72a1a74ec309b01cb66d7
PARI PATEL <paripatel3654@gmail.com>
2025-10-24 17:21:24 +0530

    Create reverseString.java

---
+++ STRINGS/reverseString.java
@@ -0,0 +1,11 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "hello";
+
+        // Efficient reversal using StringBuilder
+        String reversed = new StringBuilder(str).reverse().toString();
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversed);
+    }
+}
