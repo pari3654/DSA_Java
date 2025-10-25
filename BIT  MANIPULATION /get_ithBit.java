717c17960c400440a025043822607d9548d5b139
PARI PATEL <paripatel3654@gmail.com>
2025-10-25 16:03:08 +0530

    Create get_ithBit.java

---
+++ BIT  MANIPULATION /get_ithBit.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class bitManipulation {
+  public static int getIthBit(int n , int i) {
+    int bitmask = 1<<i;
+    if((n & bitmask) == 0) {
+        return 0;
+    } else {
+        return 1;
+    }
+
+  }
+   
+
+   
+    public static void main(String args[]) {
+        System.out.println(getIthBit(4, 2));
+    }
+}
