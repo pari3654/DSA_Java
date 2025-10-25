e9eb7cd33aeb6a0fd06bdc5697ef415065f9d7a9
PARI PATEL <paripatel3654@gmail.com>
2025-10-25 16:39:57 +0530

    Create clear_ithBit.java

---
+++ BIT  MANIPULATION /clear_ithBit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class bitManipulation {
+  public static int clearIthBit(int n , int i) {
+    int bitmask = 1<<i;
+    return n & (~(bitmask));
+  }
+   
+
+   
+    public static void main(String args[]) {
+        System.out.println(clearIthBit(10, 1));
+    }
+}
