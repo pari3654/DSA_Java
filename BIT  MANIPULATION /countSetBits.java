dcd41706d59942bf91c5a8a91fd62b946e822ea9
PARI PATEL <paripatel3654@gmail.com>
2025-10-26 17:08:35 +0530

    Create countSetBits.java

---
+++ BIT  MANIPULATION /countSetBits.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class bitManipulation {
+public static int countSetBits(int n) {
+    int count =0;
+    while(n > 0) {
+        if ((n & 1) != 0) { //check our LSB 
+          count++;
+        }
+        n = n>>1;
+    }
+    return count;
+}
+
+    public static void main(String args[]) {
+        System.out.println(countSetBits(4));
+    }
+}
