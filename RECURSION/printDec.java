7695d92ec71d87d561204d0986aa100bb5b9d79e
PARI PATEL <paripatel3654@gmail.com>
2025-11-04 19:59:48 +0530

    Implement recursive function to print numbers in descending order

---
+++ RECURSION/printDec.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Recursion{
+
+public static void printDec(int n) {
+    //base case 
+    if( n == 1 ) {
+        System.out.println(n);
+        return;
+    }
+
+    System.out.print(n+ " ");
+    printDec(n-1);
+ }
+
+public static void main(String args[]) {
+    int n = 10;
+    printDec(n);
+}
+}
