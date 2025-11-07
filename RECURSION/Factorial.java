7809c5a5886817ca82cca530bdca78acb6529963
PARI PATEL <paripatel3654@gmail.com>
2025-11-07 22:35:41 +0530

    Add recursive factorial function in Factorial.java
    Implement recursive factorial function in Java.

---
+++ RECURSION/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Recursion{
+
+public static int fact(int n ) {
+    if( n == 0 ) {
+        return 1;
+    }
+
+    int fnm1 = fact(n-1); // fn-1
+    int fn = n * fact(n-1);
+    return fn;
+}
+
+public static void main(String args[]) {
+    int n = 5;
+    System.out.println(fact(n));      
+     }
+}
