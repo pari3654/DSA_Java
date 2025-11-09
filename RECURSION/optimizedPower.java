09353066a36fc1673d6a0a99b023b2b744f618fd
PARI PATEL <paripatel3654@gmail.com>
2025-11-09 16:42:58 +0530

    Create optimizedPower.java

---
+++ RECURSION/optimizedPower.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Recursion {
+  public static int optimizedPower(int a , int n) { //O(log n)
+    if( n == 0){
+      return 1;
+    }
+
+   int  halfPower = optimizedPower(a , n/2);
+   int  halfPowerSq =  halfPower * halfPower;
+
+   //n is odd
+   if(n % 2 != 0) {
+    halfPowerSq = a * halfPowerSq;
+     }
+
+     return halfPowerSq;
+  }
+
+  public static void main(String args[]) {
+    int a = 2;
+    int n = 10;
+    System.out.println(optimizedPower(a , n));
+  }
+}
+
+
