107c22db22fea92fbcc5e57c62708171b7123a68
PARI PATEL <paripatel3654@gmail.com>
2026-03-22 23:16:58 +0530

    Create push_AtBottom.java

---
+++ STACK /push_AtBottom.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Stacks {
+
+    // push at bottom using recursion
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();          // remove element from top 
+        pushAtBottom(s, data);     // recursive call
+        s.push(top);               // vapas element daldo
+    }
+
+
+    public static void main(String args[]) {
+
+        Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtBottom(s, 4);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+}
