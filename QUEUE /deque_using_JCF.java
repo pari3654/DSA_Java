92a384531b6fb6a44b3c22ae4e3528e7533ccda8
PARI PATEL <paripatel3654@gmail.com>
2026-04-05 21:55:11 +0530

    Create deque_using_JCF.java

---
+++ QUEUE /deque_using_JCF.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Queues {
+   
+    public static void main(String args[]){
+       Deque <Integer> deque = new LinkedList<>();
+       deque.addFirst(1); //1
+       deque.addFirst(2); //2 1
+       deque.addLast(3);  // 2 1 3
+       deque.addLast(4);  //2 1 3 4
+       System.out.println(deque);
+
+       System.out.println("first element = " + deque.getFirst());
+        System.out.println("last element = " + deque.getLast());
+    }
+}
