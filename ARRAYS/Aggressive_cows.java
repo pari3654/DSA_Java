04bf3e0c2a4826af2103cf58ea746cf9dc1b9362
PARI PATEL <paripatel3654@gmail.com>
2026-04-12 22:45:55 +0530

    Update Aggressive_cows.java

--- ARRAYS/Aggressive_cows.java
+++ ARRAYS/Aggressive_cows.java
@@ -5,12 +5,13 @@ public class AggressiveCows {
     // Helper function to check if placing k cows is possible
     public static boolean canPlace(int[] stalls, int k, int guess) {
         int cows = 1; // first cow at first stall
-        int pos = stalls[0];
+        int prevpos = stalls[0];
 
         for (int i = 1; i < stalls.length; i++) {
-            if (stalls[i] - pos >= guess) {
+            int distance = stalls[i] - prevpos;
+            if (distance  >= guess) {
                 cows++;
-                pos = stalls[i];
+                prevpos = stalls[i];
             }
         }
 
@@ -18,14 +19,14 @@ public class AggressiveCows {
     }
 
     public static int aggressiveCows(int[] stalls, int k) {
-        Arrays.sort(stalls);
+        Arrays.sort(stalls); // values sorted honi chaiye
 
         int low = 1;
-        int high = stalls[stalls.length - 1] - stalls[0];
+        int high = stalls[stalls.length - 1] - stalls[0]; //last - first 
         int res = -1;
 
         while (low <= high) {
-            int mid = low + (high - low) / 2; // safe mid
+            int mid = low + (high - low) / 2; 
 
             if (canPlace(stalls, k, mid)) {
                 res = mid;        // possible → store answer
